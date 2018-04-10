83a55cadef2aabb7ce3b10aa82698a1b8e3ffd4e
Adam Selker <adamselker@gmail.com>
2018-04-10 12:02:54 -0400

    Analyze with and w/o difference

--- python/diff.py
+++ python/diff.py
@@ -8,6 +8,23 @@ def scale(img):
   scaled = np.uint8(shifted * 255.0 / np.max(shifted))
   return scaled
 
+def getLines(img, name, cannyConfig):
+
+  #Take only the red channel
+  img[:,:,0] = img[:,:,2]
+  img[:,:,1] = img[:,:,2]
+
+  scharr = cv2.Scharr(img, cv2.CV_16S, 0, 1)
+
+  cv2.imwrite(name + "-scharr.png", scale(scharr))
+
+  canny = cv2.Canny(scale(scharr), cannyConfig[0], cannyConfig[1])
+
+  # cv2.imwrite(name + "-canny.png", scale(canny))
+
+  canny_green = np.zeros(img.shape)
+  canny_green[:,:,1] = canny
+  cv2.imwrite(name + "-lines.png", scale(img + canny_green))
 
 img1 = cv2.imread(sys.argv[1])
 img2 = cv2.imread(sys.argv[2])
@@ -16,20 +33,5 @@ img2 = cv2.imread(sys.argv[2])
 diff = np.int16(img2)
 diff -= img1
 
-#Take only the red channel
-diff[:,:,0] = diff[:,:,2]
-diff[:,:,1] = diff[:,:,2]
-
-cv2.imwrite("diff.png", scale(diff))
-
-scharr = cv2.Scharr(diff, cv2.CV_16S, 0, 1)
-
-cv2.imwrite("scharr.png", scale(scharr))
-
-canny = cv2.Canny(scale(scharr), 100, 400)
-
-cv2.imwrite("canny.png", scale(canny))
-
-canny_green = np.zeros(diff.shape)
-canny_green[:,:,1] = canny
-cv2.imwrite("lines.png", scale(diff + canny_green))
+getLines(diff,"diff", (100,400))
+getLines(img2,"on",(100,400))
