e4ec3190ed1ae94a683ae5e953020ec235963027
Adam Selker <adamselker@gmail.com>
2018-04-24 11:55:17 -0400

    rather crappy Canny auto-tune.  Probably won't use it.

--- python/diff.py
+++ python/diff.py
@@ -11,16 +11,25 @@ def scale(img):
 
 def getLines(img, name, gaussSize, cannyConfig):
 
+  if cannyConfig == None: # If there's no config, we figure it out automatically.
+    sigma = 3
+
+    median = np.median(img)
+    variance = np.var(img)
+    print("Variance: " + str(median))
+    lower = -sigma * variance
+    upper = sigma * variance
+    cannyConfig = (lower,upper)
+    print("Auto config: " + str(cannyConfig))
+    # Credit for this function: https://www.pyimagesearch.com/2015/04/06/zero-parameter-automatic-canny-edge-detection-with-python-and-opencv/
+
   #Take only the red channel
   img[:,:,0] = img[:,:,2]
   img[:,:,1] = img[:,:,2]
 
-  """
-  scharr = cv2.Scharr(img, cv2.CV_16S, 0, 1)
-  cv2.imwrite(name + "-scharr.png", scale(scharr))
-  scharr_blurred = cv2.GaussianBlur(scharr,(gaussSize,gaussSize),0)
-  """
 
+  if (gaussSize%2 == 0):
+    raise ValueError("Gauss kernel size must be odd!")
   blurred = cv2.GaussianBlur(img,(gaussSize,gaussSize),0)
   scharr = cv2.Scharr(blurred, cv2.CV_16S, 0, 1)
   cv2.imwrite(name + "-scharr.png", scale(scharr))
@@ -48,5 +57,6 @@ for i in sys.argv[2:]:
   diff = np.int16(on)
   diff -= off
 
-  getLines(diff, os.path.join(name,"diff"), 51, (10,50))
-  getLines(on, os.path.join(name,"on"), 51, (10,50))
+  #getLines(diff, os.path.join(name,"diff"), 19, (160,230))
+  getLines(diff, os.path.join(name,"diff"), 19, None)
+#  getLines(on, os.path.join(name,"on"), 15, (120,170))
